c08bfd3d2d521f1053fe304d50f4caa0fc5d572d
Gabriel Cortés Garcia <tetradog@gmail.com>
2021-09-01 13:57:48 +0200

    para crear el paquete

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name="TelegramHelper",
+    version="1.0",
+    description="Sirve para ayudar en la tarea de hacer bots de telegram",
+    author="GabrielCatDeveloper",
+    author_email="gabriel.cat.developer@gmail.com",
+    url="https://github.com/TetradogBeta/TelegramHelper",
+    packages=[],
+    scripts=[]
+)
